cac052af09f165fe3c88fe8d4130728709f686f4
Jacob Lowe <jacob@jacob-UX31A.(none)>
2012-11-04 14:02:28 -0800

    wip fixing some paths

--- grunt.js
+++ grunt.js
@@ -5,14 +5,14 @@ module.exports = function(grunt) {
     concat: {
       dist: {
         src: ['src/main.js', 'src/change.js', 'src/style.js', 'src/events.js', 'src/build.js'],
-        dest: 'zoomy_build.js',
+        dest: 'build/zoomy_build.js',
         separator: '\n\n'
       }
     },
     min: {
       dist: {
-        src: ['zoomy_build.js'],
-        dest: 'zoomy_build.min.js'
+        src: ['build/zoomy_build.js'],
+        dest: 'build/zoomy_build.min.js'
       }
     },
     lint: {
