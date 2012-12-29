8e9839bb389fb908b3d5fe9188fff7d4ccc0f6e9
jacoblwe20 <jacoblowe2.0@gmail.com>
2012-12-29 15:53:24 -0800

    moving button to center to remove so functionality confusion

--- zoomy.js
+++ zoomy.js
@@ -639,7 +639,6 @@
               
             },
             //Add Button if  is touch
-            touchCallToAction = 'touch',
             magColor = '#efefef',
             icon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" overflow="inherit" xml:space="preserve">\
                 <defs>\
@@ -650,7 +649,7 @@
                   </filter>\
                 </defs>\
                 <path filter="url(#drop-shadow)" fill="' + magColor + '" d="M23.265,30.324l-9.887,9.887l-3.64-3.641l9.942-9.941c-2.687-4.42-2.134-10.246,1.668-14.049 c4.469-4.469,11.732-4.451,16.224,0.04c4.491,4.491,4.509,11.754,0.04,16.224C33.723,32.732,27.718,33.223,23.265,30.324z M24.601,15.833c-2.681,2.681-2.67,7.039,0.024,9.733s7.053,2.705,9.733,0.025c2.682-2.681,2.671-7.04-0.023-9.734 C31.641,13.162,27.282,13.152,24.601,15.833z"/>\
-              </svg>' + touchCallToAction,
+              </svg>',
 
             button = (touch) ? ['<div class="zoomy-wrap" />', '<div class=" zoomy-btn zoomy-btn-' + i + '">' + icon + '</div>'] : ['', ''];
       
